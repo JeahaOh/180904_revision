08a5280724d8784f02ab59dbb4af7cc37cc19667
JeahaOh <jeaha1217@gmail.com>
2018-09-04 20:58:12 +0900

    Split Codes by Methods

--- revision/src/main/java/App.java
+++ revision/src/main/java/App.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        //  1)  키보드 입력을 처리할 객체 준비.
-        Scanner keyIn = new Scanner(System.in);
-        
-        //  배열을 이용하여 회원 정보 저장.
-        String[] names = new String[100];
-        String[] emails = new String[100];
-        String[] passwords = new String[100];
-        
-        int index = 0;
-        
-        //  2)  반복문을 이용해서 사용자로부터 회원정보 입력 받기.
+    //  키보드 입력을 처리할 객체 준비.
+    static Scanner keyIn = new Scanner(System.in);
+    
+    //  배열을 이용하여 회원 정보 저장.
+    static String[] names = new String[100];
+    static String[] emails = new String[100];
+    static String[] passwords = new String[100];
+    
+    static int index = 0;
+    
+    static void inputMembers() {
+        //  반복문을 이용해서 사용자로부터 회원정보 입력 받기.
         while(true) {            
             System.out.print("Name : ");
             names[index] = keyIn.nextLine();
@@ -25,18 +25,25 @@ public class App {
             
             index++;
             
-            //  4)  탈출 조건
+            //  탈출 조건
             System.out.print("Continue? (Y/n)");
             String answer = keyIn.nextLine();
             if(answer.toLowerCase().equals("n")) {
                 break;
             }
-            //  3)  looping printf로 출력.
-            for(int i = 0; i < index; i ++) {
-                System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
-            }
-            
-            keyIn.close();
         }
+    }
+    
+    static void printMembers() {
+        //  looping printf로 출력.
+        for(int i = 0; i < index; i ++) {
+            System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
+        }
+    }
+    
+    public static void main(String[] args) {
+            inputMembers();
+            keyIn.close();
+            printMembers();
     }
 }
